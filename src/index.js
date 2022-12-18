7aa9507cfa0e2e7af44bc2053a0f87c7293faace
Yuliya Maslyanenko <yuliyamas86@gmail.com>
2022-12-18 20:36:24 +0200

    library

---
+++ src/index.js
@@ -0,0 +1,10 @@
+   var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "progressbar",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
