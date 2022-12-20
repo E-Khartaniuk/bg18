959b6c29556c1cee20a65ac36d5f1f553ec693c6
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2022-12-20 10:53:49 +0000

    Deploying to gh-pages from @ E-Khartaniuk/bg18@244bf5caca2a65363aabb13229bdb5e3e9ec0bf1 🚀

---
+++ index.d762974d.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",type:"progressbar"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.d762974d.js.map
---
+++ index.f8c0e806.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",type:"progressbar"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.f8c0e806.js.map
